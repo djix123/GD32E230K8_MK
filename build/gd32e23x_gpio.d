68170faf02bc9053cb125063f79fea97af76c56d
Jonathan Giles <jonathangiles@fastmail.fm>
2021-10-31 17:06:28 -0400

    first commit

---
+++ build/gd32e23x_gpio.d
@@ -0,0 +1,54 @@
+build/gd32e23x_gpio.o: \
+ Drivers/GD32E23x_standard_peripheral/Source/gd32e23x_gpio.c \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_gpio.h \
+ Drivers/CMSIS/Device/GD/GD32E23x/Include/gd32e23x.h \
+ Drivers/CMSIS/Core/Include/core_cm23.h \
+ Drivers/CMSIS/Core/Include/cmsis_version.h \
+ Drivers/CMSIS/Core/Include/cmsis_compiler.h \
+ Drivers/CMSIS/Core/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Device/GD/GD32E23x/Include/system_gd32e23x.h \
+ Inc/gd32e23x_libopt.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_adc.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_crc.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_dbg.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_dma.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_exti.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_fmc.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_syscfg.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_i2c.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_fwdgt.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_pmu.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_rcu.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_rtc.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_spi.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_timer.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_usart.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_wwdgt.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_misc.h \
+ Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_cmp.h
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_gpio.h:
+Drivers/CMSIS/Device/GD/GD32E23x/Include/gd32e23x.h:
+Drivers/CMSIS/Core/Include/core_cm23.h:
+Drivers/CMSIS/Core/Include/cmsis_version.h:
+Drivers/CMSIS/Core/Include/cmsis_compiler.h:
+Drivers/CMSIS/Core/Include/cmsis_gcc.h:
+Drivers/CMSIS/Device/GD/GD32E23x/Include/system_gd32e23x.h:
+Inc/gd32e23x_libopt.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_adc.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_crc.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_dbg.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_dma.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_exti.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_fmc.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_syscfg.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_i2c.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_fwdgt.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_pmu.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_rcu.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_rtc.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_spi.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_timer.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_usart.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_wwdgt.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_misc.h:
+Drivers/GD32E23x_standard_peripheral/Include/gd32e23x_cmp.h:
---
+++ build/startup_gd32e23x.d
@@ -0,0 +1,2 @@
+build/startup_gd32e23x.o: \
+ Drivers/CMSIS/Device/GD/GD32E23x/Source/gcc/startup_gd32e23x.s
